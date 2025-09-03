cb3675c5a7f0fe693aba8bf8795c49ef1b4ba4bc
Sebastianmedellin <smedellin@estudiante.uniajc.edu.co>
2025-09-02 20:05:13 -0500

    Se crean lo metodos prestamo, devolucion e imprimir

--- src/Libro.java
+++ src/Libro.java
@@ -15,5 +15,33 @@ public class Libro {
       this.numeroEjemplaresPrestados = numeroEjemplaresPrestados;
    }
 
+   public prestamo(int numeroEjemplares, int numeroEjemplaresPrestados){
+      boolean disponible;
+      if (numeroEjemplares>numeroEjemplaresPrestados){
+         numeroEjemplaresPrestados = numeroEjemplaresPrestados + 1;
+         disponible = true;
+      }else{
+         disponible = false;
+      }
+   }return disponible;
    
+   public devolucion(int numeroEjemplaresPrestados){
+      boolean devolucionLibro;
+      if (numeroEjemplaresPrestados>0){
+         numeroEjemplaresPrestados = numeroEjemplaresPrestados - 1;
+         devolucionLibro = true;
+      }else{
+         devolucionLibro = false;
+      }
+   }return devolucionLibro;
+
+   public void imprimir (){
+      System.out.println("El titulo del libro es: " + titulo);
+      System.out.println("El autor del libro es: " + autor);
+      System.out.println("La cantidad total de unidades del libro es: " + numeroEjemplares);
+      System.out.println("La cantidad total de unidades prestadas del libro es: " + numeroEjemplaresPrestados);
+
+    }
+
+    
 }
