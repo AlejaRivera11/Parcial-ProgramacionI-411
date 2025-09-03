000f32d868c74e720c6f5c7f0b324a25f08c05eb
Sebastianmedellin <smedellin@estudiante.uniajc.edu.co>
2025-09-02 22:06:30 -0500

    Se crea el objeto libro 1 en la rama 2 de Sebas

---
+++ src/ControladorLibros.java
@@ -0,0 +1,8 @@
+public class ControladorLibros {
+
+    public static void main(String[] args) {
+        Libro libro1 = new Libro("Cien años de soledad", "Gabriel Garcia",35,15);
+        
+    }
+
+}
